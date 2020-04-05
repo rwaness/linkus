89fe8b45a738340b162e02621dbf0d850970f4d4
Erwan O <erwan.oger35@gmail.com>
2020-04-05 16:19:09 +0200

    clean

--- src/apis/stitch/groups.ts
+++ src/apis/stitch/groups.ts
@@ -1,124 +1,126 @@
 import { BSON } from 'mongodb-stitch-browser-sdk';
 import mongodbStitch from '@/services/mongodbStitch';
 
-const collection = mongodbStitch.db.collection('groups');
-
 export const getGroupId = ({ _id: id } = {}, toString = true) => (id
   ? ((toString && `${id}`) || id)
   : null
 );
 
-export const groupsApiFactory = ({ dataType, itemToKey }) => ({
-  myInvitations: {
-    doQuery() {
-      return collection.find({
-        guests: mongodbStitch.user.profile.email,
-      }, {
-        limit: 100,
-      }).asArray();
+export const groupsApiFactory = ({ itemToKey }) => {
+  const collection = mongodbStitch.db.collection('groups');
+
+  return {
+    myInvitations: {
+      doQuery() {
+        return collection.find({
+          guests: mongodbStitch.user.profile.email,
+        }, {
+          limit: 100,
+        }).asArray();
+      },
     },
-  },
-  acceptInvitation: {
-    single: true,
-    async doQuery({ group }, storeCtx) {
-      const guest = mongodbStitch.user.profile.email;
-      const joinedGroup = await collection.findOneAndUpdate({
-        _id: getGroupId(group, false),
-        guests: guest,
-        'invitations.guest': guest,
-        memberIds: { $nin: [mongodbStitch.user.id] },
-      }, {
-        $pull: {
+    acceptInvitation: {
+      single: true,
+      async doQuery({ group }, { commit }) {
+        const guest = mongodbStitch.user.profile.email;
+        const joinedGroup = await collection.findOneAndUpdate({
+          _id: getGroupId(group, false),
           guests: guest,
-          invitations: { guest },
-        },
-        $addToSet: {
-          memberIds: mongodbStitch.user.id,
-          members: {
-            id: mongodbStitch.user.id,
-            owner: 'invitations.$.owner',
+          'invitations.guest': guest,
+          memberIds: { $nin: [mongodbStitch.user.id] },
+        }, {
+          $pull: {
+            guests: guest,
+            invitations: { guest },
           },
-        },
-      }, {
-        returnNewDocument: true,
-      });
-      if (joinedGroup) {
-        storeCtx.commit('myInvitations/removeKey', { key: itemToKey(joinedGroup) });
-        storeCtx.commit('myGroups/addKey', { key: itemToKey(joinedGroup) });
-      } else {
-        // TODO
-        alert('no group updated!');
-      }
-      return joinedGroup;
+          $addToSet: {
+            memberIds: mongodbStitch.user.id,
+            members: {
+              id: mongodbStitch.user.id,
+              owner: 'invitations.$.owner',
+            },
+          },
+        }, {
+          returnNewDocument: true,
+        });
+        if (joinedGroup) {
+          commit('myInvitations/removeKey', { key: itemToKey(joinedGroup) });
+          commit('myGroups/addKey', { key: itemToKey(joinedGroup) });
+        } else {
+          // TODO
+          alert('no group updated!');
+        }
+        return joinedGroup;
+      },
     },
-  },
-  rejectInvitation: {
-    single: true,
-    async doQuery({ group }, storeCtx) {
-      const guest = mongodbStitch.user.profile.email;
-      const rejectedGroup = await collection.findOneAndUpdate({
-        _id: getGroupId(group, false),
-        guests: guest,
-        'invitations.guest': guest,
-        memberIds: { $nin: [mongodbStitch.user.id] },
-      }, {
-        $pull: {
+    rejectInvitation: {
+      single: true,
+      async doQuery({ group }, { commit }) {
+        const guest = mongodbStitch.user.profile.email;
+        const rejectedGroup = await collection.findOneAndUpdate({
+          _id: getGroupId(group, false),
           guests: guest,
-          invitations: { guest },
-        },
-      }, {
-        returnNewDocument: true,
-      });
-      if (rejectedGroup) {
-        storeCtx.commit('myInvitations/removeKey', { key: itemToKey(rejectedGroup) });
-      } else {
-        // TODO
-        alert('no group updated!');
-      }
-      return rejectedGroup;
+          'invitations.guest': guest,
+          memberIds: { $nin: [mongodbStitch.user.id] },
+        }, {
+          $pull: {
+            guests: guest,
+            invitations: { guest },
+          },
+        }, {
+          returnNewDocument: true,
+        });
+        if (rejectedGroup) {
+          commit('myInvitations/removeKey', { key: itemToKey(rejectedGroup) });
+        } else {
+          // TODO
+          alert('no group updated!');
+        }
+        return rejectedGroup;
+      },
     },
-  },
-  myGroups: {
-    doQuery() {
-      return collection.find({
-        memberIds: mongodbStitch.user.id,
-      }, {
-        limit: 100,
-      }).asArray();
+    myGroups: {
+      doQuery() {
+        return collection.find({
+          memberIds: mongodbStitch.user.id,
+        }, {
+          limit: 100,
+        }).asArray();
+      },
     },
-  },
-  newGroup: {
-    single: true,
-    async doQuery({ name, guests }) {
-      const owner = mongodbStitch.user.id;
-      const newGroup = {
-        owner,
-        name,
-        guests,
-        invitations: guests.map((guest) => ({
-          guest,
+    newGroup: {
+      single: true,
+      async doQuery({ name, guests }) {
+        const owner = mongodbStitch.user.id;
+        const newGroup = {
           owner,
-        })),
-        memberIds: [owner],
-        members: [{
-          id: owner,
-        }],
-      };
-      const { insertedId } = await collection.insertOne(newGroup);
-      return {
-        _id: insertedId,
-        ...newGroup,
-      };
+          name,
+          guests,
+          invitations: guests.map((guest) => ({
+            guest,
+            owner,
+          })),
+          memberIds: [owner],
+          members: [{
+            id: owner,
+          }],
+        };
+        const { insertedId } = await collection.insertOne(newGroup);
+        return {
+          _id: insertedId,
+          ...newGroup,
+        };
+      },
     },
-  },
-  myGroup: {
-    single: true,
-    async doQuery({ id }, { getters }) {
-      let myGroup = getters.$item(id);
-      if (getGroupId(myGroup) !== id) {
-        myGroup = await collection.findOne({ _id: new BSON.ObjectId(id) });
-      }
-      return myGroup;
+    myGroup: {
+      single: true,
+      async doQuery({ id }, { getters }) {
+        let myGroup = getters.$item(id);
+        if (getGroupId(myGroup) !== id) {
+          myGroup = await collection.findOne({ _id: new BSON.ObjectId(id) });
+        }
+        return myGroup;
+      },
     },
-  },
-});
+  };
+};
