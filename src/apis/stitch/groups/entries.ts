03522f7e990b3e2866a417f0f6530b8b7a6b7359
Erwan O <erwan.oger35@gmail.com>
2020-04-10 13:24:08 +0200

    restructure api groups

---
+++ src/apis/stitch/groups/entries.ts
@@ -0,0 +1,124 @@
+import mongodbStitch from '@/services/mongodbStitch';
+import {
+  getGroupId,
+  getIdQuery,
+  getGuestsUpdates,
+  getInvitationsQuery,
+  getInvitationQuery,
+  getDeleteInvitationUpdates,
+} from './helpers';
+
+const collection = mongodbStitch.db.collection('groups');
+
+export default ({ itemToKey }) => ({
+  groupsList: {
+    doQuery() {
+      return collection.find({
+        memberIds: mongodbStitch.user.id,
+      }, {
+        limit: 100,
+      }).asArray();
+    },
+  },
+  newGroup: {
+    single: true,
+    async doQuery({ name, guests }) {
+      const owner = mongodbStitch.user.customData;
+      const newGroup = {
+        owner: owner.id,
+        name,
+        guests,
+        memberIds: [owner.id],
+        members: [
+          {
+            id: owner.id,
+            ...owner.profile,
+          },
+          ...getGuestsUpdates(guests, owner),
+        ],
+      };
+      const { insertedId } = await collection.insertOne(newGroup);
+      return {
+        _id: insertedId,
+        ...newGroup,
+      };
+    },
+  },
+  invite: {
+    single: true,
+    doQuery({ group, guests }) {
+      const owner = mongodbStitch.user.customData;
+      return collection.findOneAndUpdate({
+        ...getIdQuery(group.id),
+      }, {
+        $addToSet: {
+          guests: { $each: guests },
+          members: { $each: getGuestsUpdates(guests, owner) },
+        },
+      }, {
+        returnNewDocument: true,
+      });
+    },
+  },
+  myInvitations: {
+    doQuery() {
+      return collection.find({
+        ...getInvitationsQuery(),
+      }).asArray();
+    },
+  },
+  acceptInvitation: {
+    single: true,
+    async doQuery({ group }, { commit }) {
+      const joinedGroup = await collection.findOneAndUpdate({
+        ...getInvitationQuery(group),
+      }, {
+        ...getDeleteInvitationUpdates(),
+        $addToSet: {
+          memberIds: mongodbStitch.user.id,
+        },
+      }, {
+        returnNewDocument: true,
+      });
+      if (joinedGroup) {
+        const key = itemToKey(joinedGroup);
+        commit('myInvitations/removeKey', { key });
+        commit('groupsList/addKey', { key });
+      } else {
+        // TODO
+        alert('no group updated!');
+      }
+      return joinedGroup;
+    },
+  },
+  rejectInvitation: {
+    single: true,
+    async doQuery({ group }, { commit }) {
+      const rejectedGroup = await collection.findOneAndUpdate({
+        ...getInvitationQuery(group),
+      }, {
+        ...getDeleteInvitationUpdates(),
+      }, {
+        returnNewDocument: true,
+      });
+      if (rejectedGroup) {
+        const key = itemToKey(rejectedGroup);
+        commit('myInvitations/removeKey', { key });
+      } else {
+        // TODO
+        alert('no group updated!');
+      }
+      return rejectedGroup;
+    },
+  },
+  groupDetail: {
+    single: true,
+    async doQuery({ id }, { getters }) {
+      let groupDetail = getters.$item(id);
+      if (getGroupId(groupDetail) !== id) {
+        groupDetail = await collection.findOne(getIdQuery(id));
+      }
+      return groupDetail;
+    },
+  },
+});
--- src/apis/stitch/groups/helpers.ts
+++ src/apis/stitch/groups/helpers.ts
@@ -1,5 +1,30 @@
-/* eslint-disable import/prefer-default-export */
+import { BSON } from 'mongodb-stitch-browser-sdk';
+import mongodbStitch from '@/services/mongodbStitch';
+
 export const getGroupId = ({ _id: id } = {}, toString = true) => (id
   ? ((toString && `${id}`) || id)
   : null
 );
+
+export const getIdQuery = (id) => ({ _id: new BSON.ObjectId(id) });
+
+export const getInvitationsQuery = () => ({
+  guests: mongodbStitch.user.profile.email,
+  memberIds: { $nin: [mongodbStitch.user.id] },
+});
+
+export const getInvitationQuery = (group) => ({
+  _id: getGroupId(group, false),
+  ...getInvitationsQuery(),
+});
+
+export const getGuestsUpdates = (guests, owner) => guests.map((guest) => ({
+  email: guest,
+  invitedBy: owner.id,
+}));
+
+export const getDeleteInvitationUpdates = () => ({
+  $pull: {
+    guests: mongodbStitch.user.profile.email,
+  },
+});
